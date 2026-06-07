45607ab110491dc2dc90efc927c932576c935439
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-06-07 14:01:17 +0530

    E2E workflow and group metric

--- Tests/Test-05-E2eWorkflow_GroupMetric.js
+++ Tests/Test-05-E2eWorkflow_GroupMetric.js
@@ -1,23 +1,15 @@
 import { sleep, check, group } from 'k6';
 import http from 'k6/http';
 
+
+/* Below is the Complete E2E workflow: Registration, Login, Place order, Retrieve order
+
+   In real time projects, k6 is used to get the metrics for specific business workflows
+   We do this by using metrics for groups to segregate the groups' metrics
+*/
+
 const BASE_URL = "https://quickpizza.grafana.com/api";
 const PASSWORD = "admin1234";
-
-export const options = {
-
-    stages: [
-        {duration: '3s', target: 2},
-        {duration: '4s', target: 4},
-        {duration: '3s', target: 0},
-    ],
-
-    thresholds: {
-        'http_req_duration': ['p(95) < 400'],
-        'iteration_duration': ['p(95) < 4000'],
-        'checks': ['rate > 0.9'] // nearly all checks should pass
-    }
-}
 
 function generateRandomString(length) {
     const charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345678";
@@ -31,11 +23,27 @@ function generateRandomString(length) {
     return result;
 }
 
+export const options = {
+
+    stages: [
+        {duration: '3s', target: 2},
+        {duration: '4s', target: 4},
+        {duration: '3s', target: 0},
+    ],
+
+    thresholds: {
+        'checks': ['rate > 0.9'], // nearly all checks should pass
+        'group_duration{group:::Place Order Workflow}': ['p(95) < 1500']
+    }
+}
+
 export default function() {
 
     let USERNAME = "atharva " + generateRandomString(4);
     let pizzaName = "AD_Pizza " + generateRandomString(4); /* Random Pizza name */
     let orderId, authToken;
+
+    /* Groups inside the default function execute synchronously and sequentially — in the exact order they appear in code. */
 
     group('User Registration Workflow', () => {
 
@@ -97,7 +105,7 @@ export default function() {
         sleep(1);
     });
 
-    group('Create Order Workflow', () => {
+    group('Place Order Workflow', () => {
 
         let orderCreatedSuccess = false;
 
@@ -115,7 +123,8 @@ export default function() {
         };
 
         const params = {
-            headers: { "Authorization": "Bearer " + authToken, "Content-Type": "application/json" } /* token helps request identify the user */
+                        /* token helps request identify the user */
+            headers: { "Authorization": "Bearer " + authToken, "Content-Type": "application/json" }
         };
 
         const createPizzaResponse = http.post(`${BASE_URL}/pizza`, JSON.stringify(createOrderPayload), params);
@@ -168,3 +177,85 @@ export default function() {
         sleep(1);
     });
 }
+
+/* Terminal Output with this threshold
+
+
+     scenarios: (100.00%) 1 scenario, 4 max VUs, 40s max duration (incl. graceful stop):
+              * default: Up to 4 looping VUs for 10s over 3 stages (gracefulRampDown: 30s, gracefulStop: 30s)
+
+INFO[0001] User atharva ZQ1g Registered!                 source=console
+INFO[0002] User atharva ZQ1g authenticated!              source=console
+INFO[0004] Order Placed for AD_Pizza VBST!               source=console
+INFO[0004] User atharva SXWX Registered!                 source=console
+INFO[0005] Order Retrieved for AD_Pizza VBST!            source=console
+INFO[0005] User atharva SXWX authenticated!              source=console
+INFO[0006] User atharva q7H4 Registered!                 source=console
+INFO[0006] User atharva Fx15 Registered!                 source=console
+INFO[0007] Order Placed for AD_Pizza TOMN!               source=console
+INFO[0007] User atharva q7H4 authenticated!              source=console
+INFO[0008] User atharva pZXX Registered!                 source=console
+INFO[0008] User atharva Fx15 authenticated!              source=console                                                                 
+INFO[0008] Order Retrieved for AD_Pizza TOMN!            source=console                                                                 
+INFO[0008] Order Placed for AD_Pizza fDob!               source=console
+INFO[0009] Order Placed for AD_Pizza zxfK!               source=console
+INFO[0009] User atharva ouDE Registered!                 source=console
+INFO[0009] User atharva pZXX authenticated!              source=console                                                                 
+INFO[0010] Order Retrieved for AD_Pizza fDob!            source=console
+INFO[0011] Order Retrieved for AD_Pizza zxfK!            source=console
+INFO[0011] Order Placed for AD_Pizza 6DxK!               source=console
+INFO[0011] User atharva ouDE authenticated!              source=console
+INFO[0012] Order Retrieved for AD_Pizza 6DxK!            source=console
+INFO[0012] Order Placed for AD_Pizza m3A7!               source=console                                                                 
+INFO[0013] Order Retrieved for AD_Pizza m3A7!            source=console
+
+
+  █ THRESHOLDS
+
+    checks
+    ✓ 'rate > 0.9' rate=100.00%
+
+    group_duration{group:::Place Order Workflow}
+    ✓ 'p(95) < 1500' p(95)=1.33s
+
+
+  █ TOTAL RESULTS
+
+    checks_total.......: 90      6.112868/s
+    checks_succeeded...: 100.00% 90 out of 90
+    checks_failed......: 0.00%   0 out of 90
+
+    ✓ Status is 201
+    ✓ Response Contains valid UserId
+    ✓ Response Contains payload username
+    ✓ Status is 200
+    ✓ Response Contains Token
+    ✓ Token is a valid string
+    ✓ Response Contains Order id
+    ✓ Response Contains Pizza name
+    ✓ Pizza name is same as payload
+    ✓ Response Id is same as orderId stored
+
+    HTTP
+    http_req_duration..............: avg=359.07ms min=300.74ms med=363.65ms max=431.35ms p(90)=408.78ms p(95)=410.74ms
+      { expected_response:true }...: avg=359.07ms min=300.74ms med=363.65ms max=431.35ms p(90)=408.78ms p(95)=410.74ms
+    http_req_failed................: 0.00% 0 out of 24
+    http_reqs......................: 24    1.630098/s
+
+    EXECUTION
+    iteration_duration.............: avg=5.98s    min=5.44s    med=6.11s    max=6.4s     p(90)=6.34s    p(95)=6.37s
+    iterations.....................: 6     0.407525/s
+    vus............................: 1     min=1       max=4
+    vus_max........................: 4     min=4       max=4
+
+    NETWORK
+    data_received..................: 35 kB 2.4 kB/s
+    data_sent......................: 15 kB 1.0 kB/s
+
+
+
+                                                                                                                                        
+running (14.7s), 0/4 VUs, 6 complete and 0 interrupted iterations                                                                       
+default ✓ [======================================] 0/4 VUs  10s 
+
+*/
